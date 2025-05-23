3b40740a257f61e29448971c72c51a278f7fb693
NishantRaj <nishant.read@gmail.com>
2025-05-23 23:03:22 +0530

    LenovoBat 23-May-25 23:03:21.91

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Thursday, May 16, 2024 10:12 PM
 I am trying again to Understand the basic one more time.
 Best way to learn, Create it.
+
+
+Friday, May 23, 2025 10:59:22 PM
+Again Started working on it.
--- src/main/java/com/springcore/javaconfig/JavaConfig.java
+++ src/main/java/com/springcore/javaconfig/JavaConfig.java
@@ -1,8 +1,6 @@
 package com.springcore.javaconfig;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 
 public class JavaConfig {
