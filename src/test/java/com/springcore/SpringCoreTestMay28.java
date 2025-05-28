7e4b1c94adbd06975d8932d1798563eece5dd274
NishantRaj <nishant.read@gmail.com>
2025-05-28 15:34:07 +0530

    LenovoBat 28-May-25 15:34:07.66

--- src/test/java/com/springcore/SpringCoreTestMay28.java
+++ src/test/java/com/springcore/SpringCoreTestMay28.java
@@ -4,9 +4,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class SpringCoreTest {
+public class SpringCoreTestMay28 {
 	
-	public SpringCoreTest() {
+	public SpringCoreTestMay28() {
 		
 	}
 	
@@ -18,23 +18,13 @@ public class SpringCoreTest {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 		
 		
-		// This is way of creating Object of Student.
-		// You can create as many object, as you want
 		
-		Student student1 = (Student) context.getBean("Student1");
+		Student student1 = (Student) context.getBean("student");
 		System.out.println(student1);
-		
-		Student student2 = (Student) context.getBean("Student2");
-		System.out.println(student2);
-		
-		Student student3 = (Student) context.getBean("Student3");
-		System.out.println(student3);
-		
-		Student student4 = (Student) context.getBean("Student4");
-		System.out.println(student4);
 		
 		System.out.println("Testing New Style of Coding");
 		System.out.println("Testing the Style of TCS :=> ");
+		
 		((AbstractApplicationContext) context).close();
 	}
 
