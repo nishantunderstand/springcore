d29689a0fb2313057a0400391474a2c6f2ea1312
NishantRaj <nishant.read@gmail.com>
2025-05-31 13:11:14 +0530

    LenovoBat 31-May-25 13:11:14.46

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # SpringCore
 
 Tuesday, May 27, 2025 11:02 PM
-I am really thankful for TCS.
+I am really thankful for mycompany.
 That person has a lot of wisdom.
-
 
 
 Friday, May 23, 2025 10:59:22 PM
@@ -15,3 +14,14 @@ Again Started working on it.
 Thursday, May 16, 2024 10:12 PM
 I am trying again to Understand the basic one more time. Best way to learn, Create it.
 
+
+---
+
+I am referring this channel : 
+
+Learn Code with Durgesh  :
+https://www.youtube.com/playlist?list=PL0zysOflRCekeiERASkpi-crREVensZGS
+
+2022 : He give me Initial Push 
+2025 : Revisit the same Notes to become better
+Thanks Durgesh for this.
--- src/main/java/com/springcore/standalone/collections/Test.java
+++ src/main/java/com/springcore/standalone/collections/Test.java
@@ -10,14 +10,24 @@ public class Test {
 				"com/springcore/standalone/collections/aloneconfig.xml");
 
 		Person person1 = (Person) context.getBean("person1");
+		
+		System.out.println("=== List ====");
 		System.out.println(person1.getFriends());
 		System.out.println(person1.getFriends().getClass());
+		
+		System.out.println("=== Set ====");
 		System.out.println(person1.getIncome());
 		System.out.println(person1.getIncome().getClass());
+		
+		System.out.println("=== Map ====");
 		System.out.println(person1.getFeestructure());
 		System.out.println(person1.getFeestructure().getClass());
+		
+		
+		System.out.println("=== Properties ====");
 		System.out.println(person1.getProperties());
 		System.out.println(person1.getProperties().getClass());
+		System.out.println("=== END ====");
 		((AbstractApplicationContext) context).close();
 	}
 
